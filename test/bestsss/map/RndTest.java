94c3b5fef2e2c3e0c9da7c5faeb5910161f8a6a7
stanimir <stanimir@192.168.231.192>
2018-05-01 10:05:30 +0300

    RndTest with maxSize and low size

--- test/bestsss/map/RndTest.java
+++ test/bestsss/map/RndTest.java
@@ -13,18 +13,27 @@ import java.util.Map.Entry;
 public class RndTest {
   
   public static void main(String[] args) {
-    run(10000);//warm up
+    run(10000, Integer.MAX_VALUE);//warm up    
+        
+    for (int i=1; i<67; i++){
+      run((int)5.5e4, i);
+    }
     
+    timedRun((int)7.8e5, Integer.MAX_VALUE);
+  }
+
+  private static void timedRun(int iterations, int maxSize) {
     long nanos = -System.nanoTime();    
-    int s = run((int)1e6);
+    int s = run(iterations, maxSize);
     nanos += System.nanoTime();
-    System.out.printf("Time %.2fms; size: %d", BigDecimal.valueOf(nanos, 6), s);
+    System.out.printf("Time %.2fms; size: %d%n", BigDecimal.valueOf(nanos, 6), s);
   }
   
-  static int run(int iterations){    
-    int maxKey = iterations * 11;
-    float read = 0.8f;
-    float add = read + 0.15f;
+  static int run(int iterations, int maxSize){    
+    final boolean print = maxSize > 200;//skip prints for lower sizes
+    final int maxKey = iterations * 11;
+    float read = 0.8f - (maxSize < 2000? 0.2f :0f);//lower sizes prefer more "remove/put" than read
+    float add = read + (1f - read) * (maxSize <2000? .61f : .75f);//and more "remove" overall
     
     int removals = 0;
     int puts = 0;
@@ -32,6 +41,7 @@ public class RndTest {
     CompactHashMap<Integer, Long> c = new CompactHashMap<>();
     Map<Integer, Long> h = new HashMap<>();
     Random r = new Random(111);
+    
     
     for (int i=0; i< iterations; i++){
       float f = r.nextFloat();
@@ -46,7 +56,7 @@ public class RndTest {
         assertEquals(val(k), v);
         if (f < 0.01){
           assertEquals(c, h);
-          if (f < 0.0001){
+          if (print && f < 0.0001){
             System.out.print('.');//single dot; we are moving
           }
         }
@@ -59,7 +69,9 @@ public class RndTest {
         c.put(k, v);
         h.put(k, v);
         puts++;
-        continue;
+        if (c.size() <= maxSize){
+          continue;
+        }
       }
       //remove
       if (c.isEmpty())
@@ -78,8 +90,8 @@ public class RndTest {
       }
       
     }
-    
-    System.out.printf("%nIterations: %d, size: %d, puts: %d, removals: %d%n", iterations, c.size, puts, removals);
+    if (print)
+      System.out.printf("%nIterations: %d, size: %d, puts: %d, removals: %d%n", iterations, c.size, puts, removals);
     assertEquals(h, c);
     return c.size();
   }
@@ -90,11 +102,11 @@ public class RndTest {
   
 //randomKey can be used as a tool for probabilistic expiration cache (i.e. gather several random keys [+values] and expire one of them [lowest score])
   static Integer randomKey(Random r, CompactHashMap<Integer, ?> c){
-    Object[] tab = c.table;
+    final Object[] tab = c.table;
     if (tab.length == 0)
       return null;
     
-    int idx = r.nextInt(tab.length) & ~1;
+    final int idx = r.nextInt(tab.length) & ~1;
     for (int i=idx; ;){
       if (tab[i]!=null)
         return (Integer) tab[i];
